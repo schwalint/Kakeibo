6e58e9eabe573fa7547002b6f9bfb69e0db21fe5
schwalint <francohersch@gmail.com>
2026-09-25 21:26:50 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // fonts, or API calls), so caching just these few files is enough for
 // the app to load and run fully offline after the first visit.
 
-var CACHE_NAME = "kakeibo-cache-v5";
+var CACHE_NAME = "kakeibo-cache-v6";
 var FILES_TO_CACHE = [
   "./",
   "./index.html",
